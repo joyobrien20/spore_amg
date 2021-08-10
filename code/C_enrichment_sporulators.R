62f95415fb6600eac28d1a6e4486818c61c8d2c2
danschw <danschw@iu.edu>
2021-08-10 17:30:44 -0400

    arranging and adjusting HG numbers

--- code/C_enrichment_sporulators.R
+++ code/C_enrichment_sporulators.R
@@ -7,7 +7,9 @@ library(ggrepel)
 
 d.vir <- read_csv( here("data/Viruses/amg_summary_wHost.tsv")) %>% 
   # remove viruses of unknown hosts
-  filter(!is.na(order))
+  filter(!is.na(order)) %>% 
+  # remove amgs not related to sporulation
+  filter(category =="MISC")
 
 # Firmicute or not
 d.vir <- d.vir %>% 
@@ -20,13 +22,7 @@ d.vir.sum <- d.vir %>%
   summarise(n = n()) %>%
   arrange(desc(n))
 
-d.vir.sum %>% 
-  ggplot(aes(gene_id, n, color = firmicute.host))+
-  geom_point(shape=21, size=2)+
-  facet_wrap(~category, scales = "free_y")+
-  coord_flip()+
-  theme_bw()+
-  ggsave(here("plots", "AMG_x_hostTax.png"), width = 14, height = 7)
+
 
 #resolve at family level
 firmi <- d.vir %>%
@@ -35,32 +31,22 @@ firmi <- d.vir %>%
   group_by(phylum, class, family, genus) %>%
   summarise(n=n())
 
-# classification of sporulators by Galperin 2013
-spore.fam <- read_csv(here("data/Galperin_2013_MicrobiolSpectrum_table2.csv"))
-# add likelihood of sporulation
-# Galperin table fraction footnote:
-  # "The distribution of sporeformers among the 
-  # experimentally characterized members of the respective
-  # family is indicated as follows: 
-  #   +++, all (or nearly all) characterized members of the family produce spores;
-  #   ++, a significant fraction of species are sporeformers; 
-  #   +, the family includes some sporeformers; 
-  #   -, no known sporeformers in the family."
-spore.fam <- spore.fam %>% 
-  mutate(spore.likley = case_when(Fraction.spore.forming == "+++" ~ TRUE,
-                                  Fraction.spore.forming == "++" ~ TRUE,
-                                  Fraction.spore.forming == "+" ~ FALSE,
-                                  Fraction.spore.forming == "-"  ~ FALSE)) 
 
 #resolve at family level
 d.vir <-
   d.vir %>%
   separate(family.etc, into = c("family", "genus", "etc"), sep = ";",extra = "merge", remove = F) 
 
+# classification of sporulators by Galperin 2013
+# (see code at step B)
+spore.fam <- read_csv(here("data/Viruses/vMAG_host_sporul.csv"))
+
 #add Galperin sporrulation 
 d.vir <- spore.fam %>% 
-  select(Family, spore.likley) %>% 
-  left_join(d.vir, ., by = c("family" = "Family")) %>% 
+  #remove non-assigned taxids
+  filter(!is.na(phylum)) %>% 
+  select(virus.tax.id, spore.likley) %>% 
+  left_join(d.vir, ., by = "virus.tax.id") %>% 
   mutate(spore.likley = if_else(is.na(spore.likley), FALSE, spore.likley))
 # %>% group_by(firmicute.host, spore.likley) %>% summarise(n=n())
 
@@ -73,12 +59,19 @@ d.vir.sum <- d.vir %>%
 
 # spore.fam$Family %in% fams
 
+d.vir.sum %>%
+  ggplot(aes(gene_id, n, color = spore.likley))+
+  geom_point(shape=21, size=2)+
+  facet_wrap(~category, scales = "free_y")+
+  coord_flip()+
+  theme_bw()+
+  ggsave(here("plots", "AMG_x_hostTax.png"), width = 14, height = 7)
 #### Enrichment test ####
 
-n.phages <- d.vir %>% 
+n.phages <- spore.fam %>%
   # keep one record of each phage
-  filter(!duplicated(virus.tax.id)) %>% 
-  group_by(spore.likley) %>% 
+  filter(!duplicated(virus.tax.id)) %>%
+  group_by(spore.likley) %>%
   summarise(n=n())
 
 d.stat <- d.vir.sum %>% 
@@ -93,17 +86,19 @@ d.vir %>%
 
 
 # q	: the number of white balls drawn without replacement from an urn which contains both black and white balls.
-  # AMG in viruses infecting firmicutes
+  # AMG in viruses infecting sporulators
 # m	:the number of white balls in the urn.
-  # number of viruses infecting firmicutes in the pool
+  # number of viruses infecting sporulators in the pool
+m.refseq <- n.phages$n[n.phages$spore.likley]
 # n	:the number of black balls in the urn.
-  # number of viruses infecting non-firmicutes in the pool
+  # number of viruses infecting non-sporulators in the pool
+n.refseq <- n.phages$n[!n.phages$spore.likley]
 # k: the number of balls drawn from the urn, hence must be in 0,1,., m+n.
   # Total AMG detected in viruses
 
 d.stat <- d.stat %>% 
-  mutate(M = n.phages$n[n.phages$spore.likley],
-         N = n.phages$n[!n.phages$spore.likley],
+  mutate(M = m.refseq, 
+         N = n.refseq, 
          K = nonsporulator.host + sporulator.host) %>% 
   mutate(p.val = phyper(q = sporulator.host,
                         m = M, n = N, k = K, lower.tail = F, log.p = F))
@@ -144,6 +139,8 @@ d.stat %>%
                        d.stat$N[1],"phages with hosts NOT likely to sporulate,"))
   ggsave(here("plots/enrichmnent_likely_sporultor.png"), width = 6, height = 6)
   
+  
+  write_csv(d.stat, here("data","enrichment_stats.csv"))
 ##########
   # stopped here
 ##########
@@ -152,15 +149,15 @@ d.stat %>%
 # y <- dhyper(x,303,1044,280)
 # qplot(x,y)+geom_line()+geom_vline(xintercept = 54)
 # phyper(q = 9,m = 303,n = 1044,k = 10, lower.tail = F, log.p = T)
-
-
-#focus on spoIIIE
-d.vir %>% 
-  filter(str_detect(gene_description, "spoIIIE")) %>% 
-  separate(family.etc, into = c("family", "genus", "etc"), extra = "merge", fill = "right") %>% 
-  group_by(phylum,class, order, family, genus) %>% 
-  summarize(n=n())
-
-d.vir %>% 
-  filter(str_detect(gene_description, "spoIIIE")) %>% 
-  pull(virus.name)
+# 
+# 
+# #focus on spoIIIE
+# d.vir %>% 
+#   filter(str_detect(gene_description, "spoIIIE")) %>% 
+#   separate(family.etc, into = c("family", "genus", "etc"), extra = "merge", fill = "right") %>% 
+#   group_by(phylum,class, order, family, genus) %>% 
+#   summarize(n=n())
+# 
+# d.vir %>% 
+#   filter(str_detect(gene_description, "spoIIIE")) %>% 
+#   pull(virus.name)
