826d881ebb71b9e12c478b8b450e27a44f007bcd
danschw <danschw@iu.edu>
2022-01-04 21:41:15 -0500

    classify

--- gtdb_spor/code/gtdb_taxonomy.R
+++ gtdb_spor/code/gtdb_taxonomy.R
@@ -37,11 +37,15 @@ taxon_cols <- read_tsv(here("gtdb_spor/data/gtdb_downloads", f_meta), n_max = 1)
                      DataFrameCallback$new(f))
   
   d_meta_comp <- d_meta %>% 
-    select(gtdb_taxonomy, ncbi_taxonomy) %>% 
+    select(accession, gtdb_taxonomy, ncbi_taxonomy) %>% 
     separate(gtdb_taxonomy, sep = ";", into = paste0("gtdb_",c("d","p","c","o","f","g","s"))) %>% 
     separate(ncbi_taxonomy, sep = ";", into = paste0("ncbi_",c("d","p","c","o","f","g","s")))
   
-
+# list of GTDB firmicutes families
+  gtdb_fams <- d_meta_comp %>% 
+    select(gtdb_d,gtdb_p,gtdb_c,gtdb_o,gtdb_f) %>% 
+    group_by(gtdb_d,gtdb_p,gtdb_c,gtdb_o,gtdb_f) %>%
+    summarise(n=n())
 
 # Classify by Galperin ----------------------------------------------------
   # classification of sporulators by Galperin 2013
@@ -79,14 +83,14 @@ taxon_cols <- read_tsv(here("gtdb_spor/data/gtdb_downloads", f_meta), n_max = 1)
     left_join(., spore.fam %>% select(Family, spore.likely),
               by = c("ncbi_fam" = "Family"))
   
-  # is the classification consistent in GTDB taxonomy?
-  d_meta_comp %>% 
-    group_by(gtdb_f, spore.likely) %>% 
-    summarise(n=n()) %>% 
-    group_by(gtdb_f) %>% 
-    summarise(count = n()) %>% 
-    filter(count>1) %>% 
-    view()
+  # # is the classification consistent in GTDB taxonomy?
+  # d_meta_comp %>% 
+  #   group_by(gtdb_f, spore.likely) %>% 
+  #   summarise(n=n()) %>% 
+  #   group_by(gtdb_f) %>% 
+  #   summarise(count = n()) %>% 
+  #   filter(count>1) %>% 
+  #   view()
   
   p <- d_meta_comp %>% 
     filter(! gtdb_c %in% c("c__Bacilli", "c__Clostridia")) %>% 
@@ -117,6 +121,18 @@ taxon_cols <- read_tsv(here("gtdb_spor/data/gtdb_downloads", f_meta), n_max = 1)
   
   ggsave(here("gtdb_spor/plots/","Galperin_prediction_2.png"), p,
          width = 11, height = 8 )  
+  
+  # call by majority
+  gtdb_fams <- 
+    d_meta_comp %>% 
+      group_by(gtdb_f, spore.likely) %>% 
+      summarise(n = n() ) %>% 
+      pivot_wider(names_from = "spore.likely", values_from = "n", values_fill = 0) %>% 
+      mutate(total = `TRUE` + `FALSE`) %>% # total wxcluding NAs
+      mutate(perc_spor = 100*`TRUE`/total) %>% 
+      mutate(glp_spor = perc_spor > 50) %>% 
+      select(gtdb_f, glp_spor) %>% 
+      left_join(gtdb_fams, .)
 
 # Browne classification ----------------------------------------------------
   # Browne, H.P., Almeida, A., Kumar, N. et al. 
@@ -133,14 +149,70 @@ d_browne <- d_browne %>%
                     " "))
 
 
+# matching Browne to GTDB by assembly
+d_browne %>% 
+  mutate(in_meta = 
+           d_browne$Accession %>% str_remove(".*_") %>% str_remove("\\..") #remove version decimal
+         %in% 
+           (d_meta$accession %>% str_remove(".*_") %>% str_remove("\\..")))  %>% 
+  group_by(in_meta,`spore-former characterisation`) %>% 
+  summarise(n=n())
+# not all there, but there are ~1000 shared genomes, 
+# evenly split between sporulators and non=sporulators
 
+
+
+
+# add Browne classification
+d_meta_comp <- d_browne %>% 
+  mutate(join_acc = Accession %>% str_remove(".*_") %>% str_remove("\\..")) %>% 
+  select(join_acc ,brwn_spor_stat= `spore-former characterisation`) %>% 
+  left_join(d_meta_comp %>% 
+              mutate(join_acc = accession %>% str_remove(".*_") %>% 
+                       str_remove("\\..")), .)
+
+# call by majority
+gtdb_fams <- 
+  d_meta_comp %>% 
+    mutate(brwn_spor_stat = as.logical(brwn_spor_stat)) %>% 
+    group_by(gtdb_f, brwn_spor_stat) %>% 
+    summarise(n = n() ) %>% 
+    pivot_wider(names_from = "brwn_spor_stat", values_from = "n", values_fill = 0) %>% 
+    mutate(total = `TRUE` + `FALSE`) %>% # total wxcluding NAs
+    mutate(perc_spor = 100*`TRUE`/total) %>% 
+    mutate(brwn_spor = perc_spor > 50) %>% 
+    select(gtdb_f, brwn_spor) %>% 
+    left_join(gtdb_fams, .) %>% 
+    mutate(final_spor = case_when(
+      brwn_spor == TRUE ~ TRUE,
+      brwn_spor == FALSE ~ FALSE,
+      glp_spor == TRUE ~ TRUE,
+      glp_spor == FALSE ~ FALSE,
+      TRUE ~ NA
+    )) 
+
+gtdb_fams %>% 
+  group_by(final_spor) %>% 
+  summarise(fams =n(), sn = sum(n))
+
+# d_meta_comp %>% 
+#   filter(! is.na(brwn_spor_stat)) %>% 
+#   group_by(gtdb_c,gtdb_f, brwn_spor_stat) %>% 
+#   summarise(n=n()) %>% 
+#   filter(n>3) %>% 
+#   group_by(gtdb_c,gtdb_f) %>% 
+#   mutate(rel_abun = n/sum(n)) %>% 
+#   ggplot(aes(gtdb_f, rel_abun))+
+#   geom_col(aes(fill = as.character(brwn_spor_stat)))+
+#   facet_wrap(~gtdb_c, scales = "free")+
+#   theme_classic()+
+#   theme(axis.text = element_blank(),
+#         legend.position = "bottom")
   
 
+
   
-  d_meta_comp %>%
-    group_by(gtdb_c, gtdb_f) %>% 
-    summarise(n=n()) %>% 
-    arrange(desc(n)) %>% 
-    view()
+  
+
 
   
