c75de43697519420a45478a042c91aebbf0d81c2
danschw <danschw@iu.edu>
2022-03-01 14:42:48 -0500

    plot facets

--- dram_metaG/code/B1_gene_presence.R
+++ dram_metaG/code/B1_gene_presence.R
@@ -2,6 +2,7 @@
 library(here)
 library(tidyverse)
 library(egg)
+library(cowplot)
  
 # Previously, we identified homologs of sporulation genes that were enriched in
 # phages predicted to infect sporulator hosts.
@@ -14,9 +15,14 @@ library(egg)
 data_dir <-"dram_metaG/data/dram_output"
 sets <- list.dirs(here(data_dir), full.names = F, recursive = F)
 
+
+# ecosystem classification
+d.eco <- read_csv(here(data_dir, "../ecosystem_details.csv"))
+
 # remove gvd and gpd
 sets <- sets[ ! sets %in% c("Gregory_gvd","Camarillo-Guerrero")]
-
+d.eco <- d.eco %>% 
+  filter(! set %in% c("Gregory_gvd","Camarillo-Guerrero"))
 
 # Sporulation genes -------------------------------------------------------
 
@@ -76,8 +82,10 @@ for (cur_set in sets){
   annot_file <- 
     list.files(here(data_dir,cur_set), pattern = "annotation", full.names = T)
   
-  annot <- read_tsv(annot_file, col_select = "scaffold") %>% 
-    pull(1)
+  # annot <- read_tsv(annot_file, col_select = "scaffold") %>% 
+    annot <- read_tsv(annot_file) %>% 
+      select("scaffold") %>% 
+      pull(1)
   
   d.genomes <- 
     tibble(set = cur_set,
@@ -102,13 +110,12 @@ d.plot <-
   #normalize by total genes
 d.plot <-
   left_join(d.plot, d.genomes, by = "set") %>% 
-  mutate(perc_genes = 100 * n / n_genes)
+  mutate(frac_genes = n / n_genes)
   
-left_join(., total_amg) %>% 
-  mutate(perc = 100*n/n_total) %>% 
-  # 0 to na
-  filter(perc>0) %>% 
-
+  # add ecosystem type
+  d.plot <-
+    left_join(d.plot, d.eco, by = "set")
+      
 
   # total observations per gene
   obs_amg <- 
@@ -119,6 +126,9 @@ left_join(., total_amg) %>%
     mutate(obs = sum(across(where(is.numeric)))) %>% 
     arrange(desc(obs)) %>% 
     select(gene_id, obs)
+
+
+
 
   
   set_order <- 
@@ -135,7 +145,8 @@ left_join(., total_amg) %>%
 p <-
   d.plot %>% 
   # 0 to na
-  filter(perc_genes>0) %>% 
+  filter(frac_genes>0) %>% 
+  mutate(ecosystem_type = str_replace(ecosystem_type," ", "\n")) %>% 
     
   # order of Kos
   mutate(gene_id = fct_relevel(gene_id, obs_amg$gene_id)) %>%
@@ -146,10 +157,15 @@ p <-
   
   #plot
   ggplot(aes(x = gene_id, y =set))+
-  geom_tile(aes(fill  = perc_genes), color = "white")+
+  geom_tile(aes(fill  = frac_genes), color = "white")+
   #mark enriched
-  scale_fill_viridis_c(na.value = "white")+
+  scale_fill_viridis_c(na.value = "white",
+                       name='% of viral genes',
+                       breaks = c(0.001,0.002,0.003),
+                       labels = scales::percent(c(0.001,0.002,0.003), accuracy = 0.1))+
   scale_color_manual(values = c("transparent", "red"))+
+  # separate by ecosystem
+  facet_grid(ecosystem_type~., scales = "free_y", space = "free_y", switch = "y")+
   theme_classic()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
         legend.position = "bottom",
@@ -162,22 +178,26 @@ p1 <- obs_amg %>%
   geom_col()+
   theme_classic()+
   theme(axis.text.x = element_blank(),
-        axis.title.x = element_blank(),
-        axis.ticks = element_blank())
+        axis.title.x = element_blank())
 
 p2 <- d.genomes %>% 
+  left_join(., d.eco, by = "set") %>% 
+  # mutate(ecosystem_type = str_replace(ecosystem_type," ", "\n")) %>% 
   mutate(set = fct_relevel(set, set_order)) %>%
   ggplot(aes(set, n_genes))+
   geom_col()+
   coord_flip()+
+  facet_grid(ecosystem_type~., scales = "free_y", space = "free_y", switch = "y")+
   theme_classic()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
         axis.text.y = element_blank(),
-        axis.title.y = element_blank())
+        axis.title.y = element_blank(),
+        strip.text = element_blank(),
+        strip.background = element_blank())
 
 ggarrange(p1, ggplot()+ theme_void(),
           p,#+theme(axis.text.x = element_blank())+ xlab("sporulation KOs"),
-          p2+ ylab("N amg"), 
+          p2+ ylab("N genes"), 
           nrow = 2, widths = c(5,1), heights = c(1,5)) %>% 
 ggsave(here("dram_metaG/plots/","PA.png"), ., width = 8, height = 6)
 
