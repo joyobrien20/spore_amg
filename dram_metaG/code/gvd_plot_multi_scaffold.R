6e4f3a3aeb08c2554c328409d9fad33d5623c8bc
danschw <danschw@iu.edu>
2022-03-21 17:53:35 -0400

    plot scaffolds

--- dram_metaG/code/gvd_plot_multi_scaffold.R
+++ dram_metaG/code/gvd_plot_multi_scaffold.R
@@ -15,8 +15,8 @@ data_dir <- "dram_metaG/data/dram_output"
 #   "VIRSorter_AD_CERC_C7A_k121_267218_flag_0_multi_67_9926_len_109514-cat_2"
 # )
 # 
-# ko_amg <- "K07699" #spo0A
-ko_amg <- c("K06333", "K06334") #cotJ
+ko_amg <- "K07699" #spo0A
+# ko_amg <- c("K06333", "K06334") #cotJ
 cur_set <-  "Huttenhower"
 # Sporulation genes -------------------------------------------------------
 
@@ -62,8 +62,6 @@ d.amg <- d.amg %>%
                               DataFrameCallback$new(f), 
                               chunk_size = 10000) 
 
-
-
 # add index and set
 d.annot$idx <- 
   d.annot %>% 
@@ -71,6 +69,15 @@ d.annot$idx <-
   group_indices()
 
 d.annot$set <- cur_set
+
+if (!"kegg_id" %in% colnames(d.annot)){
+  if("ko_id" %in% colnames(d.annot)){
+    d.annot <- 
+      d.annot %>% 
+      rename(kegg_id = ko_id)
+  }
+}
+  
 
 
 
@@ -135,6 +142,12 @@ d.annot <- d.annot %>%
 # plot --------------------------------------------------------------------
 # https://cran.r-project.org/web/packages/gggenes/vignettes/introduction-to-gggenes.html
 
+# add vir_sorter cat
+d.annot <- 
+  d.annot %>% 
+  mutate(v.cat = str_extract(scaffold, "cat_.$")) 
+
+
 virsort_temparate <- 
   d.annot %>% 
   filter(str_detect(scaffold, "cat_4|cat_5")) %>% 
@@ -147,59 +160,80 @@ virsort_lytic <-
 
 
 
-n_genomes <- virsort_lytic[seq(1,22)] %>% sort()
-n_genomes <- virsort_lytic[seq(33,48)] %>% sort()
-n_genomes <- virsort_temparate[seq(1,22)] %>% sort()
-n_genomes <- virsort_temparate[seq(22,32)] %>% sort()
+# n_genomes <- virsort_lytic[seq(1,22)] %>% sort()
+# n_genomes <- virsort_lytic[seq(33,48)] %>% sort()
+# n_genomes <- virsort_temparate[seq(1,22)] %>% sort()
+# n_genomes <- virsort_temparate[seq(22,32)] %>% sort()
 # n_genomes <- seq(22, 22+21)
-p <- d.annot %>% 
-  filter(idx %in% n_genomes) %>% 
-  ggplot(aes(y = strandedness/3,
-             xmin = start_position, 
-             xmax = end_position,
-             fill = gene_type,
-             forward = strandedness
-  )) +
-  geom_hline(yintercept = 0, color = "black", size=0.1)+
-  geom_rect(data = d.annot %>% 
-              filter(str_detect(kegg_hit, ko_amg) | (ko_id %in% ko_amg)) %>% 
-              filter(idx %in% n_genomes),
-            aes(xmin = start_position, xmax = end_position),
-            ymin = -Inf, ymax = Inf, fill = "pink") +
-  geom_gene_arrow(color = "black") +
-  # geom_gene_arrow(data = filter(d.annot, 
-  #                               str_detect(kegg_hit, spor_ko) |(kegg_id %in% spor_ko)),
-  #                 fill = "purple") +
-  geom_gene_arrow(data = d.annot %>% 
-                    filter(str_detect(kegg_hit, ko_amg) | (ko_id %in% ko_amg)) %>% 
-                    filter(idx %in% n_genomes),
-                  aes(y=strandedness/3),
-                  size=0.5, fill = "red") +
-  geom_gene_label(aes(label = gene_position),
-                  align = "centre") +
-  facet_wrap(set ~ idx, scales = "free_x", strip.position = "left", ncol = 2,
-             labeller = label_wrap_gen()) +
-  theme_classic()+
-  panel_border(color = "black", size=0.5)+
-  theme(axis.text.y = element_blank(),
-        legend.position = "bottom",
-        # strip.background = element_blank(),
-        strip.text = element_text(size = 5),
-        axis.title.y = element_blank(),
-        axis.ticks.y = element_blank())+
-  scale_x_continuous(expand = c(0, 0))+
-  scale_fill_viridis_d()+
-  ylim(-1,1)+
-  guides(fill = guide_legend(title = "gene\ncolor"))
 
-ggsave(here("dram_metaG/plots", "cotJ_multi_genomes.png"),
-       plot = p,
-       width = 12, height = 8)
 
-# write_csv(scaf_2plot, here("dram_metaG/data/scaffolds_index.csv"))
-# write_csv(d.annot, here("dram_metaG/data/scaffolds_annotationa.csv"))
+# paginate ----------------------------------------------------------------
+row_page <- 10
+col_page <- 2
+n_pages = ceiling(max(d.annot$idx)/(row_page  * col_page) )
 
-d.annot %>% 
-  filter(idx == 56) %>% 
-  select(scaffold,gene_position, strandedness, ko_id, kegg_hit, pfam_hits,
-         hypothetical, viral_hallmark, has_annot) %>% view()
+
+for(pg in 1:n_pages){
+  p <- d.annot %>% 
+    # filter(idx %in% n_genomes) %>% 
+    ggplot(aes(y = strandedness/3,
+               xmin = start_position, 
+               xmax = end_position,
+               fill = gene_type,
+               forward = strandedness
+    )) +
+    geom_hline(yintercept = 0, color = "black", size=0.1)+
+    geom_rect(data = d.annot %>% 
+                filter(str_detect(kegg_hit, ko_amg) | (kegg_id %in% ko_amg)),# %>% 
+              # filter(idx %in% n_genomes),
+              aes(xmin = start_position, xmax = end_position),
+              ymin = -Inf, ymax = Inf, fill = "pink") +
+    geom_gene_arrow(color = "black") +
+    # geom_gene_arrow(data = filter(d.annot, 
+    #                               str_detect(kegg_hit, spor_ko) |(kegg_id %in% spor_ko)),
+    #                 fill = "purple") +
+    geom_gene_arrow(data = d.annot %>% 
+                      filter(str_detect(kegg_hit, ko_amg) | (kegg_id %in% ko_amg)),# %>% 
+                    # filter(idx %in% n_genomes),
+                    aes(y=strandedness/3),
+                    size=0.5, fill = "red") +
+    geom_gene_label(aes(label = gene_position),
+                    align = "centre") +
+    # geom_text(data = d.annot %>% select(idx, v.cat) %>% distinct(),
+    #           aes(label = v.cat), x = 1000, y=-0.95, hjust = 0, vjust = 0)+
+    # facet_wrap(set ~ paste0("#",idx,"  " , v.cat), scales = "free_x", 
+    #            strip.position = "left", ncol = 1,
+    #            labeller = label_wrap_gen()) +
+    ggforce::facet_wrap_paginate(
+      set ~ idx, 
+      scales = "free_x", strip.position = "left",
+      ncol = col_page, nrow = row_page, page = pg,
+      labeller = label_wrap_gen()
+    )+
+    theme_classic()+
+    panel_border(color = "black", size=0.5)+
+    theme(axis.text.y = element_blank(),
+          legend.position = "bottom",
+          # strip.background = element_blank(),
+          strip.text = element_text(size = 5),
+          axis.title.y = element_blank(),
+          axis.ticks.y = element_blank())+
+    scale_x_continuous(expand = c(0, 0))+
+    scale_fill_viridis_d()+
+    ylim(-1,1)+
+    guides(fill = guide_legend(title = "gene\ncolor"))
+  # 
+  file_name <- paste0("multi_genomes_p0",pg,".png")
+  ggsave(here("dram_metaG/plots", file_name),
+         plot = p,
+         width = 8, height = 11)
+}
+
+# 
+# # write_csv(scaf_2plot, here("dram_metaG/data/scaffolds_index.csv"))
+# # write_csv(d.annot, here("dram_metaG/data/scaffolds_annotationa.csv"))
+# 
+# d.annot %>% 
+#   filter(idx == 56) %>% 
+#   select(scaffold,gene_position, strandedness, ko_id, kegg_hit, pfam_hits,
+#          hypothetical, viral_hallmark, has_annot) %>% view()
